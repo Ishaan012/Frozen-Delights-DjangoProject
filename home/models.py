2f463ea016b009f50a0a3904cde85e883ff7932f
Ishaan Rawat <ishaan.rawat.ishu@gmail.com>
2024-02-07 23:47:29 +0530

    Database connected

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    firstName= models.CharField(max_length=122)
+    lastName=models.CharField(max_length=122)
+    email=models.CharField(max_length=122)
+    phoneNo=models.CharField(max_length=122)
+    message=models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -11,4 +12,15 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method=="POST":
+        firstName=request.POST.get('firstName')
+        lastName=request.POST.get('lastName')
+        email=request.POST.get('email')
+        phoneNo=request.POST.get('phoneNo')
+        message=request.POST.get('message')
+        contact=Contact(firstName=firstName, lastName=lastName,email=email,phoneNo=phoneNo,message=message)
+        contact.save()
+        def __str__(self):
+            return self.firstName
+
     return render(request,'contact.html ')
