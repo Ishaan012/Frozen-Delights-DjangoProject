1bd90c3838d8ec3959a970396c32264ad761de94
Ishaan Rawat <ishaan.rawat.ishu@gmail.com>
2024-02-08 18:36:02 +0530

    Authentication added

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("",views.index, name='home'),
     path("about",views.about, name='about'),
     path("services",views.services, name='services'),
-    path("contact",views.contact, name='contact')
+    path("contact",views.contact, name='contact'),
+    path("login",views.loginUser, name='login'),
+    path("logout",views.logoutUser, name='logout')
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from home.models import Contact
 from django.contrib import messages
-
+from django.contrib.auth.models import User
+from django.contrib.auth import logout,authenticate,login
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request,'index.html')
 
 def about(request):
@@ -11,6 +14,27 @@ def about(request):
 
 def services(request):
     return render(request,'services.html')
+
+def loginUser(request):
+    # return render(request,'login.html')
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        #Check if user has entered correct credentials
+        user=authenticate(username=username, password=password)
+        if user is not None:
+            print("request")  
+            #A backend authenticated the credentials
+            login(request,user)
+            return redirect("/")
+        else:
+            #No backend authenticated the credentials
+            return render(request,'login.html')
+    return render(request,'login.html')    
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
 
 def contact(request):
     if request.method=="POST":
@@ -24,3 +48,4 @@ def contact(request):
         messages.success(request, 'Your message has been sent !')
 
     return render(request,'contact.html ')
+
